740f0675efc54f7be5c319d9435ea27dbf98d767
arik509 <sabirhossainarik34@gmail.com>
2025-08-23 01:02:50 +0600

    Added Home Page

--- script.js
+++ script.js
@@ -1,4 +1,20 @@
 document.getElementById('btn-login').addEventListener('click',function(e){
     e.preventDefault();
-    console.log('button')
+    const mobileNumber = 12345678910;
+    const pin = 1234;
+
+    const mobileNumberValue = document.getElementById('mobile-number').value;
+
+    const mobileNumberValueConverted = parseInt(mobileNumberValue);
+
+    const pinNumberValue = document.getElementById('pin-number').value;
+
+    const pinNumberValueConverted = parseInt(pinNumberValue);
+
+    if(mobileNumberValueConverted === mobileNumber && pinNumberValueConverted === pin){
+        window.location.href="./home.html";
+    }
+    else{
+        alert('Invalid Credentials');
+    }
 })
